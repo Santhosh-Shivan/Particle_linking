49c6b518d0ab9e4aab45423b4c8aea607d68596e
Jesús Pineda <jesus.pineda@physics.gu.se>
2021-08-31 18:43:09 +0200

    Update losses.py

--- graphtrack/losses.py
+++ graphtrack/losses.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
 
-def GraphCategoricalCrossEntropy(from_logits=False, **kwargs):
+def GraphCategoricalCrossEntropy(
+    from_logits=False, loss_weights=[1, 1], **kwargs
+):
     """
     The categorical cross entropy loss function for graphs.
     ----------
@@ -9,6 +11,8 @@ def GraphCategoricalCrossEntropy(from_logits=False, **kwargs):
         If True, the input node and edge features are
         assumed to be the logits of the network, otherwise
         it is assumed to be the probabilities.
+    loss_weights : list of floats
+        The weights of the loss function for nodes and edges.
     kwargs : dict
         The keyword arguments to pass to the tensorflow
         function.
@@ -43,7 +47,7 @@ def GraphCategoricalCrossEntropy(from_logits=False, **kwargs):
         edge_loss = loss_fn(T[1], P[1])
 
         # Return the sum of the losses.
-        loss = node_loss + edge_loss
+        loss = loss_weights[0] * node_loss + loss_weights[1] * edge_loss
         return loss
 
     return loss
